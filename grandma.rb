f43d1c1d390e25e94492e944b29ae39da86f708a
Scoti Dodson <scotidodson@gmail.com>
2018-08-15 16:31:09 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(speak)
-  if speak = "I LOVE YOU GRANDMA!"
+  if speak == speak.upcase
+    puts "NO, NOT SINCE 1938!"
+  elsif speak == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
